b42cd52eddeee04762af83bd979c9bf4d3a5b9ac
yKru <ycaro00287@gmail.com>
2022-01-23 18:55:25 -0300

    Adicionamos a opcao de nao responder nada

--- dias_api/apps/Dias/models.py
+++ dias_api/apps/Dias/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Dia(models.Model):
+    NADA =     'ND'
     FRACA =    'FR'
     MODERADA = 'MD'
     FORTE =    'FT'
     INTENSA =  'IT'
 
     ESCOLHAS = [
+        (NADA, 'NADA'),
         (FRACA, 'Fraca'),
         (MODERADA, 'Moderada'),
         (FORTE, 'Forte'),
@@ -16,6 +18,6 @@ class Dia(models.Model):
     ]
 
     id_dia = models.DateField(primary_key=True)
-    tipo_fluxo = models.CharField(max_length=2, choices=ESCOLHAS, default=FRACA)
-    tipo_dor = models.CharField(max_length=2, choices=ESCOLHAS, default=FRACA)
-    tipo_TPM = models.CharField(max_length=2, choices=ESCOLHAS, default=FRACA)
+    tipo_fluxo = models.CharField(max_length=2, choices=ESCOLHAS, default=NADA)
+    tipo_dor = models.CharField(max_length=2, choices=ESCOLHAS, default=NADA)
+    tipo_TPM = models.CharField(max_length=2, choices=ESCOLHAS, default=NADA)
--- dias_api/apps/Dias/views.py
+++ dias_api/apps/Dias/views.py
@@ -20,7 +20,7 @@ class ListaDiaView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DetalhesDiaView(APIView):
